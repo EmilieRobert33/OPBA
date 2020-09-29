d496a65906c3b33ea7181f8755e01712262bed1e
Emilie <emilie.robert05@gmail.com>
2020-09-29 14:21:11 +0200

    Initial commit plus configuration api

---
+++ Api_OPBA/Extensions/ServiceExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api_OPBA.Extensions
+{
+    public static class ServiceExtensions
+    {
+        //communication avec client angular qui possede un autre domaine
+        // obligation de mettre en place la policy CORS
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+            });
+        }
+
+        //pour le deploiement sur serveur IIS
+        public static void ConfigureIISIntegration(this IServiceCollection services)
+        {
+            services.Configure<IISOptions>(options =>
+            {
+
+            });
+        }
+    }
+}
---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace Models
+{
+    /// <summary>
+    /// A CSClass definition.
+    /// </summary>
+    public sealed class ApplicationUser 
+    {
+        #region fields
+
+        #endregion
+
+        #region properties
+        public string UserId { get; set; }
+        [Display(Name ="Prénom")]
+        public string FirstName { get; set; }
+        
+        #endregion
+
+        #region constructors
+
+        public ApplicationUser()
+        {
+        }
+
+        #endregion
+
+        #region methods
+
+        #endregion
+    }
+}
